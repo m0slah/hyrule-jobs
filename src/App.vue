8880a4557e7d6bdc2b45394c8e5b58963b44e2c0
m0slah <muhammedslahqadir@gmail.com>
2023-08-29 11:42:51 +0300

    Vue Components using TypeScript

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>Hello2</div>
+  <div>
+    <p>{{ name }}</p>
+    <h2>{{ age }}</h2>
+    <button @click="changeName('mo')">change name</button>
+    <button @click="changeAge(30)">change Age</button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script lang="ts">
+let age: string | number = "25";
+
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      name: "name",
+      age: "Age" as number | string,
+    };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+      return name;
+    },
+    changeAge(age: number) {
+      this.age = age;
+      return age;
+    },
+  },
+};
+</script>
